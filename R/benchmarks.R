c5d51fa828aabfc8110422258c11933e811ffcbc
Ivan Gankevich <igankevich@ya.ru>
2017-08-14 18:31:31 +0300

    Add table with XFS, NFS, GFS benchmark results.

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -1,6 +1,6 @@
 source(file.path("build", "arma-benchmarks", "R", "arma.load.R"))
 
-arma.load_benchmark_data <- function(framework, models, tags) {
+arma.load_benchmark_data <- function(attempt, framework, models, tags) {
 	data <- data.frame()
 	for (m in models) {
 		if (!(m %in% colnames(data))) {
@@ -11,7 +11,7 @@ arma.load_benchmark_data <- function(framework, models, tags) {
 			values <- arma.load(
 				file.path("build", "arma-benchmarks", "output"),
 				"gpulab1",
-				"a4",
+        attempt,
 				10000,
 				framework,
 				m,
@@ -42,7 +42,7 @@ arma.print_openmp_vs_opencl <- function(model_names, row_names) {
   )
   all_data <- list()
   for (framework in frameworks) {
-    all_data[[framework]] <- arma.load_benchmark_data(framework, models, tags)
+    all_data[[framework]] <- arma.load_benchmark_data("a4", framework, models, tags)
   }
   # translate and pretty print in org-mode format
   saved_row_names <- rownames(all_data$openmp)
@@ -57,5 +57,54 @@ arma.print_openmp_vs_opencl <- function(model_names, row_names) {
   colnames(big_table) <- sapply(colnames(big_table), function (c) get(c, model_names))
 #  rownames(big_table) <- sapply(rownames(big_table), function (c) paste("~", c, "~", sep=""))
   print(ascii(c("", "", "", "OpenMP", "", "OpenCL")))
+  print(ascii(big_table, include.rownames=FALSE))
+}
+
+arma.load_io_benchmark_data <- function(attempt, filesystems, suffix, tags) {
+	data <- data.frame()
+	for (fs in filesystems) {
+		if (!(fs %in% colnames(data))) {
+			data[,fs] <- rep(NA, nrow(data))
+		}
+		idx <- 1
+		for (t in tags) {
+			values <- arma.load(
+				file.path("build", "arma-benchmarks", "output"),
+				"gpulab1",
+        paste(attempt, fs, suffix, sep="-"),
+				10000,
+				"openmp",
+				"ar",
+				t,
+				".*\\s+([0-9]+)us.*"
+			)
+			name <- names(tags)[idx]
+			data[if (length(name) == 0 || nchar(name) == 0) t else name,fs] <- mean(values/1000/1000)
+			idx <- idx + 1
+		}
+	}
+  data
+}
+
+arma.print_sync_vs_async_io <- function(suffix_names, row_names, top_names) {
+  library(ascii)
+  options(asciiType="org")
+  tags <- list("generate_surface", "write_all")
+  filesystems <- c("xfs", "nfs", "gfs")
+  all_data <- list()
+  for (suffix in c("seq", "par")) {
+    all_data[[suffix]] <- arma.load_io_benchmark_data("a5", filesystems, suffix, tags)
+  }
+  # translate and pretty print in org-mode format
+  saved_row_names <- rownames(all_data$seq)
+  for (suffix in names(all_data)) {
+    rownames(all_data[[suffix]]) <- 1:nrow(all_data[[suffix]])
+  }
+  big_table <- merge(all_data$seq, all_data$par, by="row.names")
+  rownames(big_table) <- saved_row_names
+  big_table[,"Row.names"] <- saved_row_names
+  big_table[,"Row.names"] <- sapply(big_table[,"Row.names"], function (c) get(c, row_names))
+  colnames(big_table) <- sapply(colnames(big_table), function (c) get(c, suffix_names))
+  print(ascii(c("", "", "", top_names[[1]], "", "",  top_names[[2]])))
   print(ascii(big_table, include.rownames=FALSE))
 }
