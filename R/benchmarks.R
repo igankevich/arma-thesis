511d33ca409b340799fd7e3715daf5f61face8ff
Ivan Gankevich <igankevich@ya.ru>
2017-08-14 19:22:29 +0300

    Add event plot for XFS, GFS, NFS benchmark.

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -1,4 +1,5 @@
 source(file.path("build", "arma-benchmarks", "R", "arma.load.R"))
+source(file.path("build", "arma-benchmarks", "R", "arma.load_events.R"))
 
 arma.load_benchmark_data <- function(attempt, framework, models, tags) {
 	data <- data.frame()
@@ -107,4 +108,73 @@ arma.print_sync_vs_async_io <- function(suffix_names, row_names, top_names) {
   colnames(big_table) <- sapply(colnames(big_table), function (c) get(c, suffix_names))
   print(ascii(c("", "", "", top_names[[1]], "", "",  top_names[[2]])))
   print(ascii(big_table, include.rownames=FALSE))
+}
+
+arma.plot_io_events <- function (fsnames) {
+  filesystems <- c("xfs", "nfs", "gfs")
+  conf <- list(
+    a=list(
+      color='#000000',
+      lty="solid",
+      lwd=3,
+      name="generate_surface"
+    ),
+    b=list(
+      color='#9F2D20',
+      lty="solid",
+      lwd=3,
+      name="write_surface"
+    )
+  )
+  for (fs in filesystems) {
+    attempt <- paste("a5", fs, "events", sep="-")
+    data <- arma.load_events(
+      file.path("build", "arma-benchmarks",
+                "output", "gpulab1", attempt, 10000, "openmp", "ar"),
+      c("write_surface", "generate_surface", "programme")
+    )
+    ev_prog <- data[data$event == "programme",]
+    ev_gen <- data[data$event == "generate_surface",]
+    ev_write <- data[data$event == "write_surface",]
+    ev_write$thread_no = max(ev_gen$thread_no) + 1
+    threads <- 0:max(ev_write$thread_no)
+    max_x <- max(ev_write$t1, ev_gen$t1)/1000/1000
+    plot.new()
+    plot.window(xlim=c(0,max_x), ylim=range(threads))
+    conf$a$table=ev_gen
+    conf$b$table=ev_write
+    for (c in conf) {
+      table <- c$table
+      for (row in seq(1,nrow(table),1)) {
+        ev <- table[row,]
+        ys <- rep(ev$thread_no, 2)
+        xs <- c(ev$t0, ev$t1)/1000/1000
+    #		points(xs[[1]], ys[[1]], pch=19, cex=0.4)
+    #		arrows(xs[[1]], ys[[1]], xs[[2]], ys[[2]], angle=10, length=0.05)
+        lines(xs, ys, lwd=3, col=c$color)
+      }
+    }
+    axis(1, at=pretty(c(0,max_x)))
+    axis(
+      2,
+      at=threads,
+      labels=c(sapply(
+        threads[1:(length(threads)-1)],
+        function (t) paste("omp", t, sep="-")
+      ), "io-0"),
+      las=2
+    )
+    mtext("Time, s", side=1, line=3)
+    mtext("Thread", side=2, line=4)
+    title(fsnames[[fs]])
+  }
+  legend(
+    "bottom",
+    inset=c(0,0.2),
+    legend=sapply(conf, function (c) c$name),
+    col=sapply(conf, function (c) c$color),
+    lty=sapply(conf, function (c) c$lty),
+    lwd=sapply(conf, function (c) c$lwd),
+    xpd=TRUE
+  )
 }
